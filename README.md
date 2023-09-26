e197627f47357be73999841734698536833bd483
rouge13 <62237564+rouge13@users.noreply.github.com>
2023-09-26 14:53:00 +0200

    Renamed elements

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# jhcv
+# jhcv-new
--- jhcv-new/wsgi.py
+++ jhcv-new/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for jhcv project.
+WSGI config for jhcv-new project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jhcv.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jhcv-new.settings')
 
 application = get_wsgi_application()
 
