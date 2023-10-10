717b9dbc857c461dcca11b7f994560f6f805ac1b
rouge13 <62237564+rouge13@users.noreply.github.com>
2023-10-10 21:43:54 +0200

    Modif static element

--- jhcv-new/settings.py
+++ jhcv-new/settings.py
@@ -123,14 +123,16 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = 'static/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    "/var/www/static/",
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static"
+# #     "/var/www/static/",
+# ]
 
 
